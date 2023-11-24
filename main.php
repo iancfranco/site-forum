0a6ae2ff6cd3c2ae408511b5df17478d2c743c2e
iancfranco <124786735+iancfranco@users.noreply.github.com>
2023-11-24 07:42:01 -0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@300&family=Dongle:wght@300&family=Inter:wght@300&family=Phudu:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="menu_lateral">
+        <div class="logodiv">
+            <h2 class="logo">Resec 🎃</h2>
+        </div>
+        <ul>
+            <li><a href="main.php">Home</a></li>
+            <li><a href="forum.php">Fórum</a></li>
+            <li><a href="arquivos.php">Arquivos</a></li>
+            <div class="profile" style="padding-top: 340%;">
+                <a href="perfil.php">
+                <h3>Perfil</h3></a>
+            </div>
+        </ul>
+    </div>
+    <div class="divatualizacoes">
+        <div class="atualizacoes">
+            <h1>Ultimas Atualizações</h1>
+        </div>
+    </div>
+    </body>
+</html>
